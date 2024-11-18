4fdf1766b71c962314884ff2fd9779937491d205
Mana <>
2024-11-18 12:02:52 +0100

    Set up for Words In Words Game

---
+++ Assets/Scripts/WordsInWordsGame/DialogueBox.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HowDoYouFeel.WordsInWordsGame
+{
+    public class DialogueBox : MonoBehaviour
+    {
+
+    }
+}
